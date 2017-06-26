f129a2cf99aeb6fe082db12cc8d925041f2144a5
Viet Doan <vietdoanhp1996@gmail.com>
2017-06-26 21:15:24 +0700

    perpective transform

---
+++ PKLot_CNN.py
@@ -0,0 +1,114 @@
+from __future__ import print_function
+import os
+from scipy import misc
+import random
+import numpy as np
+import keras
+from keras.datasets import mnist
+from keras.models import Sequential
+from keras.layers import Dense, Dropout, Flatten
+from keras.layers import Conv2D, MaxPooling2D
+from keras.layers.normalization import BatchNormalization
+
+
+file_dirs = []
+for root, dirs, files in os.walk("/data/vietdv/PKLot/PKLotSegmented", topdown=False):
+    for name in files:
+        file_dirs.append(os.path.join(root, name))
+
+
+random.shuffle(file_dirs)
+num_samples = len(file_dirs)
+X = []
+Y = []
+for i in range(0, num_samples):
+    f = misc.imread(file_dirs[i])
+    f = misc.imresize(f, (48, 64))
+    X.append(f)
+    folders = file_dirs[i].split('/')
+    label = 1 if folders[len(folders) - 2] == 'Occupied' else 0
+    Y.append(label)
+
+print("complete reading")
+
+X = np.array(X)
+Y = np.array(Y)
+(x_train, y_train) = (X[:num_samples - 150000], Y[:num_samples - 150000])
+(x_test, y_test) = (X[num_samples - 150000:num_samples - 100000], Y[num_samples - 150000:num_samples - 100000])
+
+batch_size = 128
+num_classes = 2
+epochs = 11
+img_rows, img_cols = 48, 64
+
+y_train = keras.utils.to_categorical(y_train, num_classes)
+y_test = keras.utils.to_categorical(y_test, num_classes)
+
+x_train = x_train.astype('float32')
+x_test = x_test.astype('float32')
+x_train /= 255
+x_test /= 255
+
+if keras.backend.image_data_format() == 'channels_first':
+    input_shape = (3, img_rows, img_cols)
+else:
+    input_shape = (img_rows, img_cols, 3)
+
+model = Sequential()
+model.add(Conv2D(10, kernel_size=(5, 5),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(20, kernel_size=(5, 5),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Conv2D(30, kernel_size=(5, 5),
+                 activation='relu',
+                 input_shape=input_shape))
+model.add(BatchNormalization())
+model.add(MaxPooling2D(pool_size=(2, 2)))
+
+model.add(Flatten())
+model.add(Dense(30, activation='relu'))
+model.add(Dense(20, activation='relu'))
+model.add(Dense(10, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+model.compile(loss=keras.losses.categorical_crossentropy,
+              optimizer=keras.optimizers.Adadelta(),
+              metrics=['accuracy'])
+
+model.fit(x_train, y_train,
+          batch_size=batch_size,
+          epochs=epochs,
+          verbose=1,
+          validation_data=(x_test, y_test))
+score = model.evaluate(x_test, y_test, verbose=0)
+print('Test loss:', score[0])
+print('Test accuracy:', score[1])
+
+from keras.models import load_model
+model.save('my_model_700k.h5') 
+
+x_val = []
+y_val = []
+for i in range(num_samples - 100000, num_samples):
+    f = misc.imread(file_dirs[i])
+    f = misc.imresize(f, (48, 64))
+    x_val.append(f)
+    folders = file_dirs[i].split('/')
+    label = 1 if folders[len(folders) - 2] == 'Occupied' else 0
+    y_val.append(label)
+x_val = np.array(x_val)
+y_val = np.array(y_val)
+x_val = x_val.astype('float32')
+x_val /= 255
+prediction = model.predict(x_val)
+y_val = keras.utils.to_categorical(y_val, num_classes)
+acc_val = np.mean(np.round(prediction) == y_val)
+print(acc_val)
---
+++ video.py
@@ -0,0 +1,58 @@
+import numpy as np
+import cv2
+from perpective_transform import four_point_transform
+import keras
+from keras.models import load_model
+
+li = []
+lis = []
+cap = cv2.VideoCapture('vietdv.mp4')
+model = load_model('my_model_700k.h5')
+
+def draw(event, x, y, flags, param):
+    if event == cv2.EVENT_LBUTTONUP:
+        li.append([x, y])
+
+cv2.namedWindow('image')
+cv2.setMouseCallback('image',draw)
+
+if (cap.isOpened()): 
+    ret, frame = cap.read()
+    while (1):
+        cv2.imshow('image', frame)
+        k = cv2.waitKey(1)
+        if k == 32:
+            lis.append(li)
+            pts = np.array(li, np.int32)
+            cv2.polylines(frame, [pts], True, (0,255,255))
+            li = []
+            print 'press space'
+        if k == ord('q'):
+            break
+
+def predict(img):
+    x_val = np.array([img])
+    x_val = x_val.astype('float32')
+    x_val /= 255
+    prediction = np.round(model.predict(x_val))
+    return 1 if prediction[0][1] else 0
+    
+while (cap.isOpened()):
+    ret, frame = cap.read()
+    for li in lis:
+        pts = np.array(li, np.int32)
+        warped = four_point_transform(frame, pts)
+        cv2.imshow("Warped", warped)
+        warped = cv2.resize(warped, (64, 48))
+        col = predict(warped)
+        if col == 0:
+            cv2.polylines(frame, [pts], True, (0, 255, 0))
+        else:
+            cv2.polylines(frame, [pts], True, (0, 0, 255))
+    cv2.imshow('image', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+cap.release()
+cv2.release()
+cv2.destroyAllWindows()
